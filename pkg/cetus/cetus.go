2f15edf548ec131d2eb97bd5338a1adef768acea
Andinus <andinus@nand.sh>
2020-03-18 18:00:10 +0530

    Restructure for Cetus v0.5
    This version aims for a cleaner code, better documentation & to adopt
    better practices.

--- pkg/cetus/cetus.go
+++ pkg/cetus/cetus.go
@@ -5,22 +5,9 @@ import (
 	"log"
 )
 
-var version string = "v0.4.11"
+var version string = "v0.5.0"
 
 // Version prints cetus version
 func Version() {
 	fmt.Printf("Cetus %s\n", version)
-}
-
-// PrintPath prints the path passed
-func PrintPath(path string) {
-	fmt.Println(path)
-}
-
-// ErrChk logs the context & error
-func ErrChk(ctx string, err error) {
-	if err != nil {
-		log.Println(ctx)
-		log.Fatal(err)
-	}
 }
