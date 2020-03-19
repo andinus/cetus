58122b41d2fc536e5f2ad30da70e33e59a18dc90
Andinus <andinus@nand.sh>
2020-03-19 13:22:57 +0530

    Add download function

---
+++ pkg/background/download.go
@@ -0,0 +1,34 @@
+package background
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// Download takes path and url as input and downloads the data to a
+// file, returning an error if there is one
+func Download(file string, url string) (err error) {
+	o, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer o.Close()
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("Unexpected Response: %s", res.Status)
+	}
+
+	_, err = io.Copy(o, res.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
