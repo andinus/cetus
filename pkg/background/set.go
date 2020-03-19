8cf76e6a9c04509d460e0bfd485e17abf030b6b4
Andinus <andinus@nand.sh>
2020-03-19 16:56:43 +0530

    Add support for other desktop environments

--- pkg/background/set.go
+++ pkg/background/set.go
@@ -1,13 +1,27 @@
 package background
 
-import "os/exec"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 // Set calls feh to set the background
 func Set(path string) error {
-	feh, err := exec.LookPath("feh")
-	if err != nil {
+	switch os.Getenv("XDG_CURRENT_DESKTOP") {
+	case "GNOME", "Unity", "Pantheon":
+		path = fmt.Sprintf("%s%s", "file://", path)
+		err = exec.Command("gsettings", "set org.gnome.desktop.background picture-uri", path).Run()
+		return err
+	case "LXDE":
+		err = exec.Command("pcmanfm", "-w", path).Run()
+		return err
+	default:
+		feh, err := exec.LookPath("feh")
+		if err != nil {
+			return err
+		}
+		err = exec.Command(feh, "--bg-fill", path).Run()
 		return err
 	}
-	err = exec.Command(feh, "--bg-fill", path).Run()
-	return err
 }
