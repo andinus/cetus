28e35c0b0b996e17bd94a5b9e78fda90086ba85e
Andinus <andinus@nand.sh>
2020-03-24 20:24:47 +0530

    Add cache package

---
+++ cache/getdir_unix.go
@@ -0,0 +1,29 @@
+// +build linux netbsd openbsd freebsd dragonfly
+
+package cache
+
+import (
+	"fmt"
+	"os"
+)
+
+// GetDir returns cetus cache directory. Check if the user has set
+// CETUS_CACHE_DIR, if not then check if XDG_CACHE_HOME is set & if
+// that is not set then assume it to be the default value which is
+// $HOME/.cache according to XDG Base Directory Specification.
+func GetDir() string {
+	cacheDir := os.Getenv("CETUS_CACHE_DIR")
+	if len(cacheDir) == 0 {
+		cacheDir = os.Getenv("XDG_CACHE_HOME")
+	}
+	if len(cacheDir) == 0 {
+		cacheDir = fmt.Sprintf("%s/%s", os.Getenv("HOME"),
+			".cache")
+	}
+
+	// Cetus cache directory is cacheDir/cetus.
+	cetusCacheDir = fmt.Sprintf("%s/%s", cacheDir,
+		"cetus")
+
+	return cetusCacheDir
+}
